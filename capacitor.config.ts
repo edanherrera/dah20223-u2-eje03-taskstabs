e656cb61dde602fa95e833988f2b77f9ae925167
edanherrera <edanherreraga@ittepic.edu.mx>
2022-10-23 11:11:43 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dah20223-u2-eje03-taskstabs',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
